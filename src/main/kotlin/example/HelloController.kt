7e253ef36713c1b6d7f3712275a808333e6beba0
Ryan O'Hare <ryan.ohare@nydig.com>
2022-07-07 08:25:20 -0400

    making more vulns in this b

--- src/main/kotlin/example/HelloController.kt
+++ src/main/kotlin/example/HelloController.kt
@@ -18,6 +18,9 @@ package example
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import java.io.File
+import java.util.concurrent.TimeUnit
+import java.sql.Statement
+import java.sql.DriverManager
 
 /**
  * @author James Kleeh
@@ -32,8 +35,45 @@ class HelloController(
     fun hello(name: String): String {
         var contents: String = ""
         File(name).forEachLine { contents += it }
+
+        // Command injection
+        runCommand(name)
+
+        // double SQL injection
+        validateUser(name, name)
+
+        // TODO - XSS
+
+        // TODO - CSRF
+        
         return greetingService.greet(contents)
     }
 
-    // fun hello(name: String) = greetingService.greet(name)
+    private fun runCommand(command: String) {
+        val process = Runtime.getRuntime().exec(command)
+    }
+
+    val db = DriverManager.getConnection("jdbc:sqlite::memory:").apply {
+        createStatement().apply {
+            execute("CREATE TABLE USER (ID INTEGER PRIMARY KEY, USERNAME VARCHAR(30) NOT NULL, PASSWORD VARCHAR(30) NOT NULL)")
+            execute("INSERT INTO USER (USERNAME,PASSWORD) VALUES ('thomasnield','password123')")
+            execute("INSERT INTO USER (USERNAME,PASSWORD) VALUES ('bobmarshal','batman43')")
+            close()
+        }
+    }
+
+    fun validateUser(username: String, password: String): Boolean {
+        val statement = db.createStatement()
+
+        val sql = "SELECT COUNT(*) FROM USER WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password  + "'"
+
+        println(sql)
+
+        val rs = statement.executeQuery(sql)
+        rs.next()
+
+        val result = rs.getInt(1) > 0
+        statement.close()
+        return result
+    }
 }
